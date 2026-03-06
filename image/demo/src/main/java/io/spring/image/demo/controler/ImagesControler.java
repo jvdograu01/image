1bf6bc7e6ae9fef651f7fdd8e09467ac6904b822
jvdograu01 <jvcarvalho0908@gmail.com>
2026-03-06 12:16:56 -0400

    Add files via upload

--- image/demo/src/main/java/io/spring/image/demo/controler/ImagesControler.java
+++ image/demo/src/main/java/io/spring/image/demo/controler/ImagesControler.java
@@ -1,5 +1,16 @@
 package io.spring.image.demo.controler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public class ImagesControler {
     //*
     // {"name": "", "size":100} //application/json
