099ba244c73ef4c9ede338c8334b955724b0e74a
jvdograu01 <jvcarvalho0908@gmail.com>
2026-04-17 12:31:26 -0400

    Add files via upload

--- src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
+++ src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
@@ -1,4 +1,7 @@
 package io.spring.image.demo.infra.repository;
 
-public interface ImageRepository {
+import io.spring.image.demo.domain.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image,String> {
 }
