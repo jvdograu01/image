1206b96e00dab61b7e7e2f6bfe2747da2636ca3a
jvdograu01 <jvcarvalho0908@gmail.com>
2026-05-22 12:51:39 -0400

    Add files via upload

--- demo/src/main/java/io/spring/image/demo/domain/enums/ImageExtension.java
+++ demo/src/main/java/io/spring/image/demo/domain/enums/ImageExtension.java
@@ -27,4 +27,9 @@ public enum ImageExtension {
                 .filter(ie-> ie.mediaType.equals(mediaType)).findFirst().orElse(null);
     }
 
+    public static ImageExtension ofName(String name){
+        return Arrays.stream(values())
+                .filter(ie-> ie.name().equals(name)).findFirst().orElse(null);
+    }
+
 }
--- demo/src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
+++ demo/src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
@@ -5,18 +5,39 @@ import io.spring.image.demo.domain.enums.ImageExtension;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    /**
+     *
+     * @param extension
+     * @param query
+     * @return
+     *
+     * SELECT * FROM IMAGE WHERE 1 = 1 AND EXTENSION = 'PNG' AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
+     *
+     */
+
     default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
-        Specification<Image> spec = where(conjunction());
+        Specification <Image> conjunction = (root, q, criteriaBuilder) ->criteriaBuilder.conjunction();
+        Specification<Image> spec = Specification.where(conjunction);
         if(extension !=null){
-            spec = spec.and(extensionEquals(extension));
+            //AND EXTENSION = 'PNG'
+            Specification<Image> extensionEqual = (root, q, cb) -> cb.equal(root.get("extension"), extension);
+            spec = spec.and(extensionEqual);
         }
         if(StringUtils.hasText(query)){
-            spec = spec.and(anyOf(nameLike(query), tagsLike(query)));
+            //AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
+            //Specification<Image> nameLike = (root, q, cb)-> {};
+            //Specification<Image> tagsLike = (root, q, cb)-> {};
+            Specification<Image> nameLike = (root, q, cb)-> cb.like(cb.upper(root.get("name")),"%"+query.toUpperCase()+"%");
+            Specification<Image> tagsLike = (root, q, cb)-> cb.like(cb.upper(root.get("tags")),"%"+query.toUpperCase()+"%");
 
+            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
+            spec = spec.and(nameOrTagsLike);
         }
         return findAll(spec);
     }
